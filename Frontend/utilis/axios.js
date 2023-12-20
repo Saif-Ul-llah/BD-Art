53a8508b423f7216c8f41c496d42ce3752f65f83
Saif-Ul-llah <saifhammad411@gmail.com>
2023-12-20 16:32:00 +0500

    new

--- Frontend/utilis/axios.js
+++ Frontend/utilis/axios.js
@@ -3,8 +3,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080',
-  // baseURL: 'https://cute-cyan-lion-cape.cyclic.app/',
+  // baseURL: 'http://localhost:8080',
+  baseURL: 'https://cute-cyan-lion-cape.cyclic.app/',
 
 });
 
