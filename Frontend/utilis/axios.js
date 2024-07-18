9757c18ca79236f312d4f46f9f1117b60561930b
Saif-Ul-llah <102151531+Saif-Ul-llah@users.noreply.github.com>
2024-07-18 21:51:39 +0500

    Update BaseURL

--- Frontend/utilis/axios.js
+++ Frontend/utilis/axios.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL: 'http://localhost:8080',
-  baseURL: 'https://cute-cyan-lion-cape.cyclic.app/',
+  // baseURL: 'https://cute-cyan-lion-cape.cyclic.app/',
+  baseURL:'https://node-on-vercel-nine.vercel.app/',
 
 });
 
